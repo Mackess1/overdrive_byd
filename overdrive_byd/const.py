c17f0e40354e55cff063cc542e8e4e50092da611
Mackess1 <103455283+Mackess1@users.noreply.github.com>
2026-05-06 09:58:20 -0400

    Delete overdrive_byd directory

--- overdrive_byd/const.py
+++
@@ -1,9 +0,0 @@
-DOMAIN = "overdrive_byd"
-
-CONF_TOPIC = "topic"
-CONF_NAME = "name"
-
-DEFAULT_TOPIC = "overdrive/vehicle/telemetry"
-DEFAULT_NAME = "BYD Car"
-
-PLATFORMS = ["sensor", "binary_sensor", "device_tracker"]
