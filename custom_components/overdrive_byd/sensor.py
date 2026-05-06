070dfaa83655c48e30a46ec8408922acbad7df5a
Mackess1 <103455283+Mackess1@users.noreply.github.com>
2026-05-06 10:49:55 -0400

    Update sensor.py

--- custom_components/overdrive_byd/sensor.py
+++ custom_components/overdrive_byd/sensor.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone
 from homeassistant.components import mqtt
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
 from homeassistant.const import UnitOfTemperature, UnitOfLength, PERCENTAGE
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity import DeviceInfo, EntityCategory
 from homeassistant.helpers.dispatcher import async_dispatcher_send, async_dispatcher_connect
 
 from .const import DOMAIN, CONF_TOPIC, CONF_NAME
@@ -14,22 +14,24 @@ _LOGGER = logging.getLogger(__name__)
 
 
 SENSORS = [
-    ("utc", "UTC", None, None),
-    ("soc", "Battery Percentage", PERCENTAGE, SensorDeviceClass.BATTERY),
-    ("power", "Power", "kW", SensorDeviceClass.POWER),
-    ("speed", "Speed", "km/h", None),
-    ("lat", "Latitude", None, None),
-    ("lon", "Longitude", None, None),
-    ("elevation", "Elevation", UnitOfLength.METERS, None),
-    ("ext_temp", "Outside Temperature", UnitOfTemperature.CELSIUS, SensorDeviceClass.TEMPERATURE),
-    ("batt_temp", "Battery Temperature", UnitOfTemperature.CELSIUS, SensorDeviceClass.TEMPERATURE),
-    ("odometer", "Odometer", UnitOfLength.KILOMETERS, SensorDeviceClass.DISTANCE),
-    ("soh", "Battery Health", PERCENTAGE, None),
-    ("capacity", "Battery Capacity", "kWh", None),
-    ("gear", "Gear", None, None),
-    ("consumption_50km", "Consumption 50km", "kWh/100km", None),
-    ("driving_time_hours", "Driving Time", "h", None),
-    ("ev_range_km", "EV Range", UnitOfLength.KILOMETERS, SensorDeviceClass.DISTANCE),
+    ("utc", "Last Update", None, None, EntityCategory.DIAGNOSTIC),
+    ("soc", "Battery Percentage", PERCENTAGE, SensorDeviceClass.BATTERY, None),
+    ("power", "Power", "kW", SensorDeviceClass.POWER, None),
+    ("speed", "Speed", "km/h", None, None),
+
+    ("lat", "Latitude", None, None, EntityCategory.DIAGNOSTIC),
+    ("lon", "Longitude", None, None, EntityCategory.DIAGNOSTIC),
+    ("elevation", "Elevation", UnitOfLength.METERS, None, EntityCategory.DIAGNOSTIC),
+
+    ("ext_temp", "Outside Temperature", UnitOfTemperature.CELSIUS, SensorDeviceClass.TEMPERATURE, None),
+    ("batt_temp", "Battery Temperature", UnitOfTemperature.CELSIUS, SensorDeviceClass.TEMPERATURE, None),
+    ("odometer", "Odometer", UnitOfLength.KILOMETERS, SensorDeviceClass.DISTANCE, None),
+    ("soh", "Battery Health", PERCENTAGE, None, EntityCategory.DIAGNOSTIC),
+    ("capacity", "Battery Capacity", "kWh", None, EntityCategory.DIAGNOSTIC),
+    ("gear", "Gear", None, None, None),
+    ("consumption_50km", "Consumption 50km", "kWh/100km", None, None),
+    ("driving_time_hours", "Driving Time", "h", None, EntityCategory.DIAGNOSTIC),
+    ("ev_range_km", "EV Range", UnitOfLength.KILOMETERS, SensorDeviceClass.DISTANCE, None),
 ]
 
 
@@ -46,18 +48,22 @@ async def async_setup_entry(hass, entry, async_add_entities):
             return
 
         hass.data[DOMAIN][entry.entry_id]["data"] = payload
+        hass.data[DOMAIN][entry.entry_id]["last_seen"] = datetime.now(timezone.utc)
         async_dispatcher_send(hass, signal)
 
     unsub = await mqtt.async_subscribe(hass, topic, message_received, 0)
     hass.data[DOMAIN][entry.entry_id]["listeners"].append(unsub)
 
     async_add_entities(
-        [OverdriveBYDSensor(entry, name, signal, key, label, unit, device_class) for key, label, unit, device_class in SENSORS]
+        [
+            OverdriveBYDSensor(entry, name, signal, key, label, unit, device_class, entity_category)
+            for key, label, unit, device_class, entity_category in SENSORS
+        ]
     )
 
 
 class OverdriveBYDSensor(SensorEntity):
-    def __init__(self, entry, vehicle_name, signal, key, label, unit, device_class):
+    def __init__(self, entry, vehicle_name, signal, key, label, unit, device_class, entity_category):
         self.entry = entry
         self.vehicle_name = vehicle_name
         self.signal = signal
@@ -66,6 +72,7 @@ class OverdriveBYDSensor(SensorEntity):
         self._attr_unique_id = f"{entry.entry_id}_{key}"
         self._attr_native_unit_of_measurement = unit
         self._attr_device_class = device_class
+        self._attr_entity_category = entity_category
 
     @property
     def device_info(self):
@@ -83,7 +90,7 @@ class OverdriveBYDSensor(SensorEntity):
 
         if self.key == "utc" and value:
             try:
-                return datetime.fromtimestamp(value, timezone.utc).isoformat()
+                return datetime.fromtimestamp(value, timezone.utc).strftime("%Y-%m-%d %I:%M:%S %p")
             except Exception:
                 return value
 
