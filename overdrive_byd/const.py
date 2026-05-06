c664d922b5bf65e0228b6e4f246926adafb9a356
Mackess1 <103455283+Mackess1@users.noreply.github.com>
2026-05-06 09:47:38 -0400

    Add files via upload

---
+++ overdrive_byd/const.py
@@ -0,0 +1,9 @@
+DOMAIN = "overdrive_byd"
+
+CONF_TOPIC = "topic"
+CONF_NAME = "name"
+
+DEFAULT_TOPIC = "overdrive/vehicle/telemetry"
+DEFAULT_NAME = "BYD Car"
+
+PLATFORMS = ["sensor", "binary_sensor", "device_tracker"]
