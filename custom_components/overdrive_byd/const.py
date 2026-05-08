2312aff16173a71e438c5ac053fc3cae82bbc169
Mackess1 <103455283+Mackess1@users.noreply.github.com>
2026-05-08 11:56:50 -0400

    Update const.py

--- custom_components/overdrive_byd/const.py
+++ custom_components/overdrive_byd/const.py
@@ -1,9 +1,19 @@
 DOMAIN = "overdrive_byd"
 
 CONF_TOPIC = "topic"
+CONF_COMMAND_TOPIC = "command_topic"
 CONF_NAME = "name"
 
 DEFAULT_TOPIC = "overdrive/vehicle/telemetry"
+DEFAULT_COMMAND_TOPIC = "overdrive/vehicle/command"
 DEFAULT_NAME = "BYD Car"
 
-PLATFORMS = ["sensor", "binary_sensor", "device_tracker"]
+PLATFORMS = [
+    "sensor",
+    "binary_sensor",
+    "device_tracker",
+    "button",
+    "lock",
+    "climate",
+    "cover",
+]
