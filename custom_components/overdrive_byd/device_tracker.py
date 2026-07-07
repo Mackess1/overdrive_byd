ab542f193667ceb8ea74cd9d0c8c4b1c11101ef2
Mackess1 <103455283+Mackess1@users.noreply.github.com>
2026-07-06 23:00:21 -0400

    Update device_tracker.py

--- custom_components/overdrive_byd/device_tracker.py
+++ custom_components/overdrive_byd/device_tracker.py
@@ -1,59 +1,41 @@
-from homeassistant.components.device_tracker.config_entry import TrackerEntity
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
-from homeassistant.helpers.entity import DeviceInfo
+from __future__ import annotations
 
-from .const import CONF_NAME, DOMAIN
+from homeassistant.components.device_tracker.config_entry import TrackerEntity
+
+from .const import DOMAIN
+from .entity import OverdriveBYDEntity, clean_value
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    name = entry.data[CONF_NAME]
-    signal = f"{DOMAIN}_{entry.entry_id}_update"
-
-    async_add_entities([OverdriveBYDTracker(entry, name, signal)])
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities([OverdriveBYDDeviceTracker(coordinator)])
 
 
-class OverdriveBYDTracker(TrackerEntity):
-    def __init__(self, entry, vehicle_name, signal):
-        self.entry = entry
-        self.vehicle_name = vehicle_name
-        self.signal = signal
+class OverdriveBYDDeviceTracker(OverdriveBYDEntity, TrackerEntity):
+    _attr_name = "Location"
 
-        self._attr_name = f"{vehicle_name} Location"
-        self._attr_unique_id = f"{entry.entry_id}_location"
-
-    @property
-    def device_info(self):
-        return DeviceInfo(
-            identifiers={(DOMAIN, self.entry.entry_id)},
-            name=self.vehicle_name,
-            manufacturer="BYD",
-            model="Overdrive MQTT Vehicle",
-        )
+    def __init__(self, coordinator) -> None:
+        super().__init__(coordinator)
+        self._attr_unique_id = "overdrive_byd_location"
 
     @property
     def latitude(self):
-        data = self.hass.data[DOMAIN][self.entry.entry_id]["data"]
-        return data.get("lat")
+        return clean_value(self.coordinator.data.get("lat"))
 
     @property
     def longitude(self):
-        data = self.hass.data[DOMAIN][self.entry.entry_id]["data"]
-        return data.get("lon")
-
-    @property
-    def battery_level(self):
-        data = self.hass.data[DOMAIN][self.entry.entry_id]["data"]
-        return data.get("soc")
+        return clean_value(self.coordinator.data.get("lon"))
 
     @property
     def source_type(self):
         return "gps"
 
-    async def async_added_to_hass(self):
-        self.async_on_remove(
-            async_dispatcher_connect(
-                self.hass,
-                self.signal,
-                self.async_write_ha_state,
-            )
-        )
+    @property
+    def extra_state_attributes(self):
+        return {
+            "vin": self.coordinator.data.get("vin"),
+            "speed": clean_value(self.coordinator.data.get("speed")),
+            "elevation": clean_value(self.coordinator.data.get("elevation")),
+            "utc": clean_value(self.coordinator.data.get("utc")),
+            "vd_timestamp": clean_value(self.coordinator.data.get("vd_timestamp")),
+        }
